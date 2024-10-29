7e137fa200c68dbf2c075b1c22a29f7529998193
Tzafrir Rehan <tzafrir.r@gmail.com>
2024-10-29 16:32:48 +0200

    Replace localhost with STREAMLIT_URL and VNC_URL environment variables in HTML files
    * Update iframe source URLs in `index.html` and `static_content/index.html` to use `STREAMLIT_URL` and `VNC_URL`
    * Add code in `entrypoint.sh` to search and replace `STREAMLIT_URL` and `VNC_URL` in `index.html` and `static_content/index.html`

--- computer-use-demo/image/entrypoint.sh
+++ computer-use-demo/image/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+# Replace placeholders in index.html and static_content/index.html with environment variables
+sed -i "s/\${STREAMLIT_URL}/${STREAMLIT_URL}/g" index.html
+sed -i "s/\${VNC_URL}/${VNC_URL}/g" index.html
+sed -i "s/\${STREAMLIT_URL}/${STREAMLIT_URL}/g" static_content/index.html
+sed -i "s/\${VNC_URL}/${VNC_URL}/g" static_content/index.html
+
 ./start_all.sh
 ./novnc_startup.sh
 
